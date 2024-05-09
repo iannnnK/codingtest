bc56e5ea494cd2c761b9baa0707a9f8e2e90ae39
ian <ian@soulware.kr>
2024-05-09 13:16:38 +0900

    update(Problem5): 풀이 수정

--- src/Problem4.java
+++ src/Problem4.java
@@ -1,26 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Problem4 {
     private static String number;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
 
-        // 날짜를 여러 값으로 전달할 수 있으면 좋을 듯
-        calcSunday(getFirstDayOfWeek(), calcMonthInfo());
+        calcSunday(input);
     }
 
-    private static int getFirstDayOfWeek() {
-        return LocalDate.now().withDayOfMonth(1).getDayOfWeek().getValue();
+    private static int getFirstDayOfWeek(LocalDate localDate) {
+        return localDate.withDayOfMonth(1).getDayOfWeek().getValue();
     }
 
-    private static int calcMonthInfo() {
-        LocalDate now = LocalDate.now();
-        boolean leapYear = now.isLeapYear();
-        return now.getMonth().length(leapYear);
+    private static int calcMonthInfo(LocalDate localDate) {
+        boolean leapYear = localDate.isLeapYear();
+        return localDate.getMonth().length(leapYear);
     }
 
-    private static void calcSunday(int startDayOfWeek, int countOfDay) {
+    private static void calcSunday(String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate inputDate = LocalDate.parse(input, formatter);
+
+        int startDayOfWeek = getFirstDayOfWeek(inputDate);
+        int countOfDay = calcMonthInfo(inputDate);
+
         int count = 0;
         for (int i = startDayOfWeek; i < startDayOfWeek + countOfDay; i++) {
             if (i % 7 == 0) {
