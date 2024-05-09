c534679399eacc236fa048dd29b33a31cecf8fb4
ian <ian@soulware.kr>
2024-05-09 13:24:38 +0900

    update(Problem6): 콘솔 입력 추가

--- src/Problem5.java
+++ src/Problem5.java
@@ -1,7 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Problem5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // x^2 + y^2 = r^2
-        int r = 20;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int r = Integer.parseInt(input);
+
+        drawCircle(r);
+    }
+
+    private static void drawCircle(int r) {
         for (int i = -r; i <= r; i++) {
             for (int j = -r; j <= r; j++) {
 
