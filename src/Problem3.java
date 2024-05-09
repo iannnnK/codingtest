4a082c24a9c701d104ebc241fd0ecf73954ec367
ian <ian@soulware.kr>
2024-05-09 13:02:10 +0900

    update(Problem3): swap 방식으로 풀이 추가

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,25 +1,49 @@
+import java.lang.reflect.Array;
 import java.security.SecureRandom;
-import java.util.Random;
+import java.util.*;
 
 public class Problem3 {
     public static void main(String[] args) {
         String input = "1234567890";
-        boolean[] visited = new boolean[input.length()];
 
-        SecureRandom r = new SecureRandom();
-        Random random = new Random();
-        String answer = "";
+        // SecureRandom 사용
+        System.out.println(createRandomStr(input));
+        // Collections.shuffle() 사용
+        System.out.println(shuffleStr(input));
+    }
 
-        // 불필요한 반복이 있음
-        while(answer.length() < input.length()) {
-            int now = random.nextInt(input.length());
-            if(visited[now]) {
-                continue;
-            }
+    private static String createRandomStr(String input) {
+        SecureRandom random = new SecureRandom();
 
-            visited[now] = true;
-            answer += input.charAt(now);
+        char[] arr = input.toCharArray();
+        int size = input.length();
+        String result = "";
+        for(int i = 0; i < size; i++) {
+            int idx = random.nextInt(size);
+            char t = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = t;
         }
-        System.out.println(answer);
+
+        for(int i = 0; i < arr.length; i++) {
+            result += arr[i];
+        }
+        return result;
+    }
+
+    // secure 하지는 않지만, Collections.shuffle을 이용한 비교적 간단한 풀이방법
+    private static String shuffleStr(String input) {
+        List<Character> list = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        String answer = "";
+        Collections.shuffle(list);
+        for (int i = 0; i < list.size(); i++) {
+            answer += list.get(i);
+        }
+
+        return answer;
     }
 }
